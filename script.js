d90f15248047d48ae2064d36c11b5f4858f517b8
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-02-04 21:23:12 +0200

    fi

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ const password = document.querySelector('#password')
 const confirmPassword = document.querySelector('#confirm-password')
 const submitButton = document.querySelector('.submit-button')
 const passwordDontMatch= document.querySelector('#dont-match')
+const phone = document.querySelector('#phone')
+const passwordBorder= document.querySelector('#password')
+const confirmPasswordBorder = document.querySelector('#confirm-password')
 
-
-
-// submitButton.addEventListener('click',() =>{
-//     if (password.value !== confirmPassword.value)
-        
-// })
 
 confirmPassword.addEventListener('input',() =>{
-    if (password.value === confirmPassword.value)
+    if (password.value === confirmPassword.value){
+        passwordBorder.style.border = '2px solid rgb(96, 137, 197)';
+        confirmPasswordBorder.style.border = '2px solid rgb(96, 137, 197)';       
         return passwordDontMatch.textContent = ''
+    }
+
     else if (password.value.length > 7){
-        passwordDontMatch.textContent = '*Passwords do not match'
-        passwordDontMatch.style.color = 'red'
+        passwordDontMatch.textContent = '*Passwords do not match';
+        passwordDontMatch.style.color = 'red';
+        passwordBorder.style.border = '2px solid #ff0000';
+        confirmPasswordBorder.style.border = '2px solid #ff0000';
     }    
 })
 
